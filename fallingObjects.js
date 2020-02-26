a5caa03e24e4314775527e833cbd7adfdca9d0f0
Slav Gagarov <sgagar2@uic.edu>
2020-02-26 18:28:52 +0000

    falling rasberries

---
+++ fallingObjects.js
@@ -0,0 +1,23 @@
+class  PositiveObject{
+  constructor(x, y, w, h, pValue, speed, src) {
+    this.x = x;
+    this.y = y;
+    this.width=w;
+    this.height=h;
+    this.pointValue = pValue;
+    this.speed=speed;
+    this.alive = true;
+  }
+    
+  onCollision() {
+    player.score+=speed;
+  }
+    
+  drawFallingObject() {
+    ctx.beginPath();
+    this.y+=this.speed;
+    if(this.y > canvas.height)
+        this.alive = false; 
+    ctx.drawImage(this.imgSrc, this.x, this.y, this.width, this.height);
+  }
+}
---
+++ player.js
@@ -0,0 +1,29 @@
+let playersize = canvas.width / 6;
+
+let player = {
+    x: canvas.width / 2,
+    y: canvas.height - playersize * 1.5,
+    size: playersize,
+    height: playersize * 1.5,
+    width: playersize,
+    speed: canvas.width / 100,
+    action: "stay",
+    score: 0
+}
+
+function drawPlayer() {
+    ctx.beginPath();
+    switch(player.action) {
+        case "moveRight":
+            player.x += player.speed;
+            break;
+        case "moveLeft":
+            player.x -= player.speed;
+            break;
+    }
+    if(player.x > canvas.width - player.size) 
+        player.x = canvas.width - player.size;
+    if(player.x < 0) 
+        player.x = 0;
+    ctx.drawImage(playerAvatar, player.x, player.y, player.width, player.height);
+}

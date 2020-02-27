6364a49c248822aaa3ddf947eaeb20a002774d87
Slav Gagarov <sgagar2@uic.edu>
2020-02-27 00:14:50 +0000

    collisions

--- fallingObjects.js
+++ fallingObjects.js
@@ -1,5 +1,5 @@
-class  PositiveObject{
-  constructor(x, y, w, h, pValue, speed, src) {
+class  FallingObject{
+  constructor(x, y, w, h, pValue, speed, src, id) {
     this.x = x;
     this.y = y;
     this.width=w;
@@ -8,11 +8,10 @@ class  PositiveObject{
     this.speed=speed;
     this.imgSrc = src;
     this.alive = true;
+    this.id=id;
   }
     
-  onCollision() {
-    player.score+=speed;
-  }
+  onCollision() {}
     
   drawFallingObject() {
     ctx.beginPath();
@@ -20,5 +19,50 @@ class  PositiveObject{
     if(this.y > canvas.height)
         this.alive = false; 
     ctx.drawImage(this.imgSrc, this.x, this.y, this.width, this.height);
+  }
+  
+  getID(){
+  	return this.id;
+  }
+  
+  
+  checkPlayerCollisions(){
+	var pX=player.x+player.width*0.6;
+	var pW=player.width*0.05;
+	var pY=player.y;
+	var pH=player.height;
+  
+	var tX=this.x;// + 10;
+	var tW=this.width;// - 10;
+	var tY=this.y;
+	var tH=this.height;
+  	if(pX < tX + tW &&
+  		pX + pW > tX &&
+  		pY < tY + tH &&
+  		pY + pH > tY)
+  		{
+  			this.onCollision();
+  			this.alive = false; 
+  		}
+  }
+}
+
+class PositiveObject extends FallingObject{
+	constructor(x, y, w, h, pValue, speed, src, id) {
+		super(x, y, w, h, pValue, speed, src, id);
+  }
+
+	onCollision() {
+    player.score+=this.pointValue;
+  }
+}
+
+class NegativeObject extends FallingObject{
+	constructor(x, y, w, h, speed, src, id) {
+		super(x, y, w, h, 0, speed, src, id);
+  }
+  
+  onCollision() {
+  	//play.lives--;
   }
 }
